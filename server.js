a2ff16ed0bd468704e69c9039538db98772dd740
Ab108singh <abhaysingh24071@gmail.com>
2024-08-01 23:40:03 +0530

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
