e4a3c59e068cd61266bf9178864a84c31879b692
Ab108singh <abhaysingh24071@gmail.com>
2024-08-02 01:43:34 +0530

    ..

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 // controllers/userController.js
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require("bcrypt")
 exports.register = async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const user = new User({ username, password });
+    console.log("hii");
+    const { email, password } = req.body;
+    const user = new User({ email, password });
     await user.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
@@ -15,13 +16,25 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    console.log("hii1");
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '10d' });
+    res.json({ user,token });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.varify = async (req, res, next) => {
+  try {
+    let token = req.headers.token;
+    
+    let {userId} =jwt.verify(token,process.env.JWT_SECRET);
+    let user = await User.findById(userId);
+    res.json({user})
   } catch (error) {
     next(error);
   }
--- models/User.js
+++ models/User.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role:{type:String,default:"user"}
 });
 
 userSchema.pre('save', async function (next) {
